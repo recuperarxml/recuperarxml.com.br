c3ca56043802c9f75adc78f3e7a2a6b94e0c508b
Leonardo Gregianin <leogregianin@gmail.com>
2016-04-13 23:47:36 -0400

    Some changes

--- gulpfile.js
+++ gulpfile.js
@@ -16,25 +16,25 @@ gulp.task('rx', function() {
 
     // js
     gulp.src(['src/js/**/*.js'])
-        .pipe(uglify()).pipe(gulp.dest('dist/ext/js'));      
+        .pipe(uglify()).pipe(gulp.dest('dist/assets/js'));      
     // css 
-    gulp.src('src/css/**/*.css')
-        .pipe(cssnano()).pipe(gulp.dest('dist/ext/css'));
+    gulp.src('assets/css/**/*.css')
+        .pipe(cssnano()).pipe(gulp.dest('dist/assets/css'));
     // copiar
     gulp.src('*.ico')
         .pipe(gulp.dest('dist'));
-    gulp.src('src/img/*.{png,jpg,svg}')
-        .pipe(gulp.dest('dist/ext/img'));
-    gulp.src('src/css/fonts/*.{eot,svg,ttf,woff}')
-        .pipe(gulp.dest('dist/ext/css/fonts'));
-    gulp.src('src/css/less/*.less')
-        .pipe(gulp.dest('dist/ext/css/less'));
-    gulp.src('src/css/font-awesome-4.1.0/fonts/*.{otf,eot,svg,ttf,woff}')
-        .pipe(gulp.dest('dist/ext/css/font-awesome-4.1.0/fonts'));
-    gulp.src('src/css/font-awesome-4.1.0/less/*.less')
-        .pipe(gulp.dest('dist/ext/css/font-awesome-4.1.0/less'));
-    gulp.src('src/css/font-awesome-4.1.0/scss/*.scss')
-        .pipe(gulp.dest('dist/ext/css/font-awesome-4.1.0/scss'));
+    gulp.src('assets/img/*.{png,jpg,svg}')
+        .pipe(gulp.dest('dist/assets/img'));
+    gulp.src('assets/css/fonts/*.{eot,svg,ttf,woff}')
+        .pipe(gulp.dest('dist/assets/css/fonts'));
+    gulp.src('assets/css/less/*.less')
+        .pipe(gulp.dest('dist/assets/css/less'));
+    gulp.src('assets/css/font-awesome-4.1.0/fonts/*.{otf,eot,svg,ttf,woff}')
+        .pipe(gulp.dest('dist/assets/css/font-awesome-4.1.0/fonts'));
+    gulp.src('assets/css/font-awesome-4.1.0/less/*.less')
+        .pipe(gulp.dest('dist/assets/css/font-awesome-4.1.0/less'));
+    gulp.src('assets/css/font-awesome-4.1.0/scss/*.scss')
+        .pipe(gulp.dest('dist/assets/css/font-awesome-4.1.0/scss'));
 });
 
 // TODO: inserir task para envio ao ftp com vinyl-ftp
