6c4fa0bde4e43a6827214cc510e63e883fa59ed2
Leonardo Gregianin <leogregianin@gmail.com>
2016-04-24 22:13:27 -0400

    added recuperarxml.less install gulp-less

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,15 @@ var cssnano = require('gulp-cssnano');
 var gutil   = require('gulp-util');
 var uglify  = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
+var less    = require('gulp-less');
 
 gulp.task('rx', function() {
     
+    // compile LESS
+    gulp.src('assets/css/recuperarxml.less')
+        .pipe(less())
+        .pipe(gulp.dest('assets/css'));
+
     // html
     gulp.src(['index.html',
               'google9593787cc7c11ddd.html',
@@ -15,15 +21,17 @@ gulp.task('rx', function() {
         .pipe(gulp.dest('dist'));
 
     // js
-    gulp.src(['src/js/**/*.js'])
+    gulp.src(['assets/js/**/*.js'])
         .pipe(uglify()).pipe(gulp.dest('dist/assets/js'));      
+
     // css 
     gulp.src('assets/css/**/*.css')
         .pipe(cssnano()).pipe(gulp.dest('dist/assets/css'));
-    // copiar
+
+    // copy do ./dist
     gulp.src('*.ico')
         .pipe(gulp.dest('dist'));
-    gulp.src('assets/img/*.{png,jpg,svg}')
+    gulp.src('assets/img/*.{png,jpg,svg,gif}')
         .pipe(gulp.dest('dist/assets/img'));
     gulp.src('assets/css/fonts/*.{eot,svg,ttf,woff}')
         .pipe(gulp.dest('dist/assets/css/fonts'));
@@ -35,4 +43,15 @@ gulp.task('rx', function() {
         .pipe(gulp.dest('dist/assets/css/font-awesome-4.1.0/less'));
     gulp.src('assets/css/font-awesome-4.1.0/scss/*.scss')
         .pipe(gulp.dest('dist/assets/css/font-awesome-4.1.0/scss'));
+
 });
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(['index.html', 
+              'assets/js/**/*.js', 
+              'assets/css/**/*.less'], ['rx']);
+});
+
+// Default task
+gulp.task('default', ['watch','rx']);
